a947e82ba280d653abb75690f48bd12d27be6866
Criid <Lowry_46@hotmail.com>
2023-10-24 13:21:37 -0400

    added js file and linked it in the layout file.

---
+++ app/static/scripts/scripts.js
@@ -0,0 +1,20 @@
+// Change this to the ID of the list when set
+var list = document.getElementById('myList');
+
+// Get all the list items in the list
+var items = list.getElementsByTagName('li');
+
+// Loop through each list item
+for (var i = 0; i < items.length; i++) {
+  // Add a click event listener to the current list item
+  items[i].addEventListener('click', function() {
+    // Check the current value of the text-decoration property
+    if (this.style.textDecoration === 'line-through') {
+      // If it's 'line-through', remove the strikethrough style
+      this.style.textDecoration = 'none';
+    } else {
+      // Otherwise, add the strikethrough style
+      this.style.textDecoration = 'line-through';
+    }
+  });
+}
